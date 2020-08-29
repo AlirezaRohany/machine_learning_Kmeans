ed4412e7445c08636c2824dfa1dee3f4448ec11c
Alireza Rohany <alireza.rohany.77@gmail.com>
2020-08-29 14:37:14 +0430

    Initial commit

---
+++ K-means.py
@@ -0,0 +1,9 @@
+import sklearn
+from sklearn.cluster import KMeans
+from sklearn.datasets import load_digits
+from sklearn.preprocessing import scale
+import tensorflow
+import keras
+import numpy
+
+print("K-means is real")
